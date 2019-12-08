0bf5f8724ccc485791949d124ed2545b7ac3cbc6
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2019-12-08 20:57:10 +0100

    Add DbContext Class to Infrastructure

---
+++ ImportScheduler.Infrastructure/ImportSchedulerDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace ImportScheduler.Infrastructure
+{
+    public class ImportSchedulerDbContext : DbContext
+    {
+
+
+        public ImportSchedulerDbContext(DbContextOptions<ImportSchedulerDbContext> options)
+            : base(options)
+        {
+
+        }
+
+    }
+}
+    
