d2e5f66befa1d8c7d02987fbda8cf5ba64ee91e9
Philipp C. Moser <info@philipp-moser.de>
2019-12-13 00:51:20 +0100

    Testing: Implement sample data-fetching from Server

--- ImportScheduler.Web/ClientApp/src/app/home/home.component.ts
+++ ImportScheduler.Web/ClientApp/src/app/home/home.component.ts
@@ -1,8 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
+
 export class HomeComponent {
+  public forecasts: WeatherForecast[];
+
+  constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
+    http.get<WeatherForecast[]>(baseUrl + 'weatherforecast').subscribe(result => {
+      this.forecasts = result;
+    }, error => console.error(error));
+  }
+}
+
+interface WeatherForecast {
+  date: string;
+  temperatureC: number;
+  temperatureF: number;
+  summary: string;
 }
