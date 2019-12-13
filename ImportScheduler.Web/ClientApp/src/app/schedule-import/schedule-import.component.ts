176806b4ee22f04307cd2a2141b92a67f13aa15f
Philipp C. Moser <info@philipp-moser.de>
2019-12-13 23:30:13 +0100

    Add new Component for ClientApp

---
+++ ImportScheduler.Web/ClientApp/src/app/schedule-import/schedule-import.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'schedule-import-view',
+  templateUrl: './schedule-import.component.html',
+})
+export class ScheduleImportComponent {
+}
