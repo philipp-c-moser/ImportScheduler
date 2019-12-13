132891c903adff0de5e5e0fc8b47b40deb5a5ae8
Philipp C. Moser <info@philipp-moser.de>
2019-12-13 21:30:11 +0100

    Add User Model to DbContext

--- ImportScheduler.Infrastructure/Data/ImportSchedulerDbContext.cs
+++ ImportScheduler.Infrastructure/Data/ImportSchedulerDbContext.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.EntityFrameworkCore;
 
+using ImportScheduler.Core.Domain.CsvImport;
+
 namespace ImportScheduler.Infrastructure.Data
 {
     public class ImportSchedulerDbContext : DbContext
     {
 
+        public DbSet<User> Users { get; set; }
 
         public ImportSchedulerDbContext(DbContextOptions<ImportSchedulerDbContext> options)
             : base(options)
