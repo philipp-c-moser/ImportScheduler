03076d9c59f8191bf529317a9d5faac91efd991b
Philipp C. Moser <info@philipp-moser.de>
2019-12-14 18:54:47 +0100

    ClientApp: Update Structure for Components

--- ImportScheduler/ClientApp/src/app/app-routing.module.ts
+++ ImportScheduler/ClientApp/src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { ScheduleImportComponent } from './schedule-import/schedule-import/schedule-import.component';
+import { HomeComponent } from './components/home/home.component';
+import { ScheduleImportComponent } from './components/schedule-import/schedule-import/schedule-import.component';
 
 
 const routes: Routes = [
