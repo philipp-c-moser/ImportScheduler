4bbf9f4beb50b618e1dfc76ba4b07687687f255b
Philipp C. Moser <info@philipp-moser.de>
2019-12-14 20:01:45 +0100

    Add new Component: ImportHistoryComponent

--- ImportScheduler/ClientApp/src/app/app.module.ts
+++ ImportScheduler/ClientApp/src/app/app.module.ts
@@ -11,12 +11,14 @@ import { NavMenuComponent } from './components/nav-menu/nav-menu.component';
 import { HomeComponent } from './components/home/home.component';
 import { ScheduleImportComponent } from './components/schedule-import/schedule-import/schedule-import.component';
 import { ScheduleImportFormComponent } from './components/schedule-import/schedule-import-form/schedule-import-form.component';
+import { ImportHistoryComponent } from './components/import-history/import-history.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavMenuComponent,
     HomeComponent,
+    ImportHistoryComponent,
     ScheduleImportComponent,
     ScheduleImportFormComponent
   ],
