ce3d3bac9237a0221ca7ab7665aa0ad03154b661
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2019-12-16 20:00:35 +0100

    Implement IUnitOfWork in UnitOfWork

---
+++ ImportScheduler/Infrastructure/UnitOfWork.cs
@@ -0,0 +1,32 @@
+﻿using ImportScheduler.Core;
+using ImportScheduler.Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ImportScheduler.Infrastructure
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+
+        public readonly ImportSchedulerDbContext _context;
+
+        public UnitOfWork(ImportSchedulerDbContext context)
+        {
+            _context = context;
+        }
+
+
+        public void Complete()
+        {
+            _context.SaveChanges();
+        }
+
+        public async Task CompleteAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+
+    }
+}
