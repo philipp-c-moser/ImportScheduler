16913cb06adaad78a0fa8045fb1c3d571753150d
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2019-12-16 21:35:32 +0100

    Add datatable and provide demo-data. Added new Scss File for user-overview.component

--- ImportScheduler/ClientApp/src/app/components/user-overview/user-overview.component.ts
+++ ImportScheduler/ClientApp/src/app/components/user-overview/user-overview.component.ts
@@ -3,6 +3,37 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'user-overview',
   templateUrl: './user-overview.component.html',
+  styleUrls: ['./user-overview.component.scss']
 })
 export class UserOverviewComponent {
+
+  rows = [
+    { firstname: 'Reginald', lastname: 'Edison', gender: 'Male', email: 'ReginaldAEdison@einrot.com', mobilenumber: '206-808-8464' },
+    { firstname: 'Reginald', lastname: 'Edison', gender: 'Male', email: 'ReginaldAEdison@einrot.com', mobilenumber: '206-808-8464' },
+    { firstname: 'Reginald', lastname: 'Edison', gender: 'Male', email: 'ReginaldAEdison@einrot.com', mobilenumber: '206-808-8464' },
+    { firstname: 'Reginald', lastname: 'Edison', gender: 'Male', email: 'ReginaldAEdison@einrot.com', mobilenumber: '206-808-8464' },
+    { firstname: 'Reginald', lastname: 'Edison', gender: 'Male', email: 'ReginaldAEdison@einrot.com', mobilenumber: '206-808-8464' },
+    { firstname: 'Reginald', lastname: 'Edison', gender: 'Male', email: 'ReginaldAEdison@einrot.com', mobilenumber: '206-808-8464' },
+    { firstname: 'Reginald', lastname: 'Edison', gender: 'Male', email: 'ReginaldAEdison@einrot.com', mobilenumber: '206-808-8464' },
+    { firstname: 'Reginald', lastname: 'Edison', gender: 'Male', email: 'ReginaldAEdison@einrot.com', mobilenumber: '206-808-8464' },
+    { firstname: 'Reginald', lastname: 'Edison', gender: 'Male', email: 'ReginaldAEdison@einrot.com', mobilenumber: '206-808-8464' },
+    { firstname: 'Reginald', lastname: 'Edison', gender: 'Male', email: 'ReginaldAEdison@einrot.com', mobilenumber: '206-808-8464' },
+    { firstname: 'Reginald', lastname: 'Edison', gender: 'Male', email: 'ReginaldAEdison@einrot.com', mobilenumber: '206-808-8464' },
+    { firstname: 'Reginald', lastname: 'Edison', gender: 'Male', email: 'ReginaldAEdison@einrot.com', mobilenumber: '206-808-8464' },
+  ];
+
+  loadingIndicator = true;
+  reorderable = true;
+
+
+  columns = [
+    { prop: 'firstname' },
+    { prop: 'lastname' },
+    { prop: 'gender', minWidht: 50 },
+    { prop: 'email', minWidth: 300 },
+    { prop: 'mobilenumber', sortable: false }
+  ];
+
+
+
 }
