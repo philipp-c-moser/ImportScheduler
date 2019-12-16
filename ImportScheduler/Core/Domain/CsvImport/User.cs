ef0b092b4d3497d73e37beb32ed53c415cc15bdf
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2019-12-16 19:23:24 +0100

    Update User Model

--- ImportScheduler/Core/Domain/CsvImport/User.cs
+++ ImportScheduler/Core/Domain/CsvImport/User.cs
@@ -6,5 +6,8 @@ namespace ImportScheduler.Core.Domain.CsvImport
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public char Gender { get; set; }
+        public string Email { get; set; }
+        public string MobileNumber { get; set; }
     }
 }
