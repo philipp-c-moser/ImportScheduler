3b3fc3a7c39bbb927f27854c25eff6d7bd7311a7
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2019-12-16 22:24:44 +0100

    First approach of controller.
    Use AutoMapper in the future.

--- ImportScheduler/Controllers/UsersController.cs
+++ ImportScheduler/Controllers/UsersController.cs
@@ -4,6 +4,10 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using ImportScheduler.Core;
+using ImportScheduler.Core.Repository;
+using ImportScheduler.Core.Dto.CsvImport;
+
 
 namespace ImportScheduler.Controllers
 {
@@ -11,5 +15,41 @@ namespace ImportScheduler.Controllers
     [ApiController]
     public class UsersController : ControllerBase
     {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IUserRepository _userRepository;
+
+
+        public UsersController(IUnitOfWork unitOfWork, IUserRepository userRepository)
+        {
+            _unitOfWork = unitOfWork;
+            _userRepository = userRepository;
+        }
+
+
+
+        public async Task<IEnumerable<UserDto>> GetUsers()
+        {
+            var allUsers = await _userRepository.GetAllAsync();
+
+            var result = new List<UserDto>();
+
+
+            foreach(var user in allUsers)
+            {
+                result.Add(new UserDto()
+                {
+                    Id = user.Id,
+                    FirstName = user.FirstName,
+                    LastName = user.LastName,
+                    Gender = user.Gender,
+                    Email = user.Email,
+                    MobileNumber = user.MobileNumber
+                });
+            }
+
+            return result;
+        }
+
+
     }
 }
