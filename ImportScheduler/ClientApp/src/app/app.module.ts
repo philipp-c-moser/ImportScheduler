6908677d4cbd1ff68a4527124c0c6e3b82aab4ab
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2019-12-16 20:52:22 +0100

    Update app.module.ts

--- ImportScheduler/ClientApp/src/app/app.module.ts
+++ ImportScheduler/ClientApp/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { RouterModule } from '@angular/router';
+
 
 import { AppRoutingModule } from './app-routing.module';
 
