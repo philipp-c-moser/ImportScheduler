6506cc69c7227e4bbf7b27f3f8f008ac5a518276
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2019-12-16 19:35:09 +0100

    Create UserDto.cs

---
+++ ImportScheduler/Core/Dto/CsvImport/UserDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ImportScheduler.Core.Dto.CsvImport
+{
+    public class UserDto
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public char Gender { get; set; }
+        public string Email { get; set; }
+        public string MobileNumber { get; set; }
+    }
+}
