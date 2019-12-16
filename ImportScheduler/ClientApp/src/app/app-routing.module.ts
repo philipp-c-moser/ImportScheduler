e1d9aeed37b3e485c064706f9f2ce6b35eb886ff
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2019-12-16 20:34:40 +0100

    Add UserOverview to Routing Module

--- ImportScheduler/ClientApp/src/app/app-routing.module.ts
+++ ImportScheduler/ClientApp/src/app/app-routing.module.ts
@@ -3,13 +3,15 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { ScheduleImportComponent } from './components/schedule-import/schedule-import/schedule-import.component';
 import { ImportHistoryComponent } from './components/import-history/import-history.component';
+import { UserOverviewComponent } from './components/user-overview/user-overview.component';
 
 
 const routes: Routes = [
   
   { path: '', component: HomeComponent, pathMatch: 'full' },
-  { path: 'ImportHistory', component: ImportHistoryComponent},
-  { path: 'ScheduleImport', component: ScheduleImportComponent}
+  { path: 'ImportHistory', component: ImportHistoryComponent },
+  { path: 'ScheduleImport', component: ScheduleImportComponent },
+  { path: 'UserOverview', component: UserOverviewComponent }
 
 ];
 
