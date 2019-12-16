e12d32ba5c2d57798c701d7af85952fadee7225b
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2019-12-16 22:45:22 +0100

    Create MappingProfile Class and implement first Profile

---
+++ ImportScheduler/Infrastructure/Mapping/MappingProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using ImportScheduler.Core.Domain.CsvImport;
+using ImportScheduler.Core.Dto.CsvImport;
+
+namespace ImportScheduler.Infrastructure.Mapping
+{
+    public class MappingProfile : Profile
+    {
+
+        public MappingProfile()
+        {
+            CreateMap<User, UserDto>();
+        }       
+
+
+    }
+}
