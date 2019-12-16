13fef4363889862b25493b336720d3c4f7afe026
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2019-12-16 19:57:09 +0100

    Create IUnitOfWork.cs

---
+++ ImportScheduler/Core/IUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ImportScheduler.Core
+{
+    public interface IUnitOfWork
+    {
+
+        void Complete();
+
+        Task CompleteAsync();
+
+    }
+}
