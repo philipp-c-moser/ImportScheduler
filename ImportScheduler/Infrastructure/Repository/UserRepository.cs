271e004d9e780c99282a7e47897145e64f10c642
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2019-12-16 20:05:34 +0100

    Implement UserRepository

---
+++ ImportScheduler/Infrastructure/Repository/UserRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ImportScheduler.Core.Domain.CsvImport;
+using ImportScheduler.Core.Repository;
+using ImportScheduler.Infrastructure.Data;
+
+namespace ImportScheduler.Infrastructure.Repository
+{
+    public class UserRepository : Repository<User>, IUserRepository
+    {
+
+        public UserRepository(ImportSchedulerDbContext context)
+            : base(context)
+        {
+
+        }
+
+    }
+}
