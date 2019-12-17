47998cf41c088d4154d951bfbc48267596d60730
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2019-12-17 18:56:50 +0100

    Add "getUsers()" Method

--- ImportScheduler/ClientApp/src/app/services/user-overview.service.ts
+++ ImportScheduler/ClientApp/src/app/services/user-overview.service.ts
@@ -1,11 +1,18 @@
 import { Http } from '@angular/http';
 import { Injectable } from '@angular/core';
+import  { map } from 'rxjs/operators';
 
 
 @Injectable()
 export class UserOverviewService {
 
     constructor(private http: Http) {}
+
+
+    getUsers() {
+        return this.http
+            .get('/api/users').map(res => res.json());
+    }
 
 
 }
