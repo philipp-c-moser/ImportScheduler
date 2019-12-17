86d28a91ef7ff634c6766833145eec1099423e37
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2019-12-18 00:21:11 +0100

    Implemented Upload in angular application

--- ImportScheduler/ClientApp/src/app/components/schedule-import/schedule-import-form/schedule-import-form.component.ts
+++ ImportScheduler/ClientApp/src/app/components/schedule-import/schedule-import-form/schedule-import-form.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
+import { Http } from '@angular/http';
 import { ScheduleImportService } from 'src/app/services/schedule-import.service';
+import { HttpRequest, HttpEventType, HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'schedule-import-form',
@@ -7,24 +9,28 @@ import { ScheduleImportService } from 'src/app/services/schedule-import.service'
 })
 export class ScheduleImportFormComponent {
 
-  constructor(private scheduleImportService: ScheduleImportService) { }
 
+  constructor(private http: HttpClient) { }
 
   upload(files) {
 
-    if(files.length === 0) 
+    if (files.length === 0)
     {
       return;
     }
 
     const formData = new FormData();
 
-    for(let file of files) 
+    for (let file of files) 
     {
       formData.append(file.name, file);
     }
 
-    this.scheduleImportService.uploadFile(formData);
+
+    // TODO: Move to shedule-import.service.ts
+    const uploadHttpRequest = new HttpRequest('POST', '/api/import', formData);
+
+    this.http.request(uploadHttpRequest).subscribe(event => { });
     
   }
 
