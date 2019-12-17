66fa8a8923ec393d398b59543ee67fb7e161614e
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2019-12-17 20:53:37 +0100

    Include IHostingEnvironment and add webRootPath

--- ImportScheduler/Controllers/ImportController.cs
+++ ImportScheduler/Controllers/ImportController.cs
@@ -12,12 +12,14 @@ namespace ImportScheduler.Controllers
     {
         private readonly ImportSchedulerDbContext _context;
         private readonly IUnitOfWork _unitOfWork;
+        private readonly IHostingEnvironment _hostingEnvironment;
 
 
-        public ImportController(ImportSchedulerDbContext context, IUnitOfWork unitOfWork)
+        public ImportController(ImportSchedulerDbContext context, IUnitOfWork unitOfWork, IHostingEnvironment hostingEnvironment)
         {
             _context = context;
             _unitOfWork = unitOfWork;
+            _hostingEnvironment = hostingEnvironment;
         }
 
 
@@ -28,6 +30,7 @@ namespace ImportScheduler.Controllers
             try
             {
                 var file = Request.Form.Files[0];
+                var webRootPath = _hostingEnvironment.WebRootPath;
 
 
                 return Ok("Upload successful");
