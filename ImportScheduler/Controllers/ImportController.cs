b2619c91a91d1ca251eedeafcbe943aead41322c
Philipp C. Moser <46461566+philipp-c-moser@users.noreply.github.com>
2019-12-17 20:30:40 +0100

    Set Up ImportController

---
+++ ImportScheduler/Controllers/ImportController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ImportScheduler.Core;
+using ImportScheduler.Infrastructure.Data;
+
+namespace ImportScheduler.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImportController : ControllerBase
+    {
+        private readonly ImportSchedulerDbContext _context;
+        private readonly IUnitOfWork _unitOfWork;
+
+
+        public ImportController(ImportSchedulerDbContext context, IUnitOfWork unitOfWork)
+        {
+            _context = context;
+            _unitOfWork = unitOfWork;
+        }
+
+
+
+    }
+}
